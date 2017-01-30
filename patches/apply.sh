9553f7c4ef458ef161e0ca201cfa4fdb35581af7
Joe <joe2k01dev@gmail.com>
2017-01-30 14:59:35 +0100

    I'm building with ubuntu 16.XX based distro

--- patches/apply.sh
+++ patches/apply.sh
@@ -1,3 +1,4 @@
+rootdirectory="$PWD"
 patch -Np0 -i device/htc/buzz/patches/apns-conf.xml.patch
 patch -Np0 -i device/htc/buzz/patches/armv6j.mk.patch
 patch -Np0 -i device/htc/buzz/patches/browser.patch
@@ -9,3 +10,7 @@ patch -Np0 -i device/htc/buzz/patches/libagl.patch
 patch -Np0 -i device/htc/buzz/patches/LOCAL_MODULE_TAGS.patch
 patch -Np0 -i device/htc/buzz/patches/ota-no-recovery.patch
 patch -Np0 -i device/htc/buzz/patches/surfaceflinger.patch
+cd $rootdirectory
+cd dalvik/
+echo "Applying dalvik patch"
+git apply $rootdirectory/device/htc/buzz/patches/compatibility.patch
